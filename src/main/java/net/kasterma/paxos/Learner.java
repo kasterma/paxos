47cca1d482907f029b09c4823685bd6317fce237
Bart Kastermans <kasterma@kasterma.net>
2018-03-20 07:11:01 +0100

    Reduce learning chatter

--- src/main/java/net/kasterma/paxos/Learner.java
+++ src/main/java/net/kasterma/paxos/Learner.java
@@ -20,6 +20,12 @@ public class Learner extends AbstractActor {
     private final FiniteDuration duration =
             Duration.create(1, TimeUnit.SECONDS);
 
+    /**
+     * Store the chosen value here; remaind null as long as we have not
+     * discovered the chosen value yet.
+     */
+    private Integer chosenValue = null;
+
     static Props props(final List<ActorRef> acceptorList) {
         return Props.create(Learner.class, acceptorList);
     }
@@ -39,7 +45,12 @@ public class Learner extends AbstractActor {
 
     private void checkValue() {
         log.info("Check if value has been decicded");
-        acceptorList.forEach(a -> a.tell(new Acceptor.Accepted(), getSelf()));
+        if (chosenValue != null) {
+            acceptorList
+                    .forEach(a -> a.tell(new Acceptor.Accepted(), getSelf()));
+            // and immediately schedule another check.
+            scheduleCheckValue(duration);
+        }
     }
 
     /**
@@ -52,23 +63,24 @@ public class Learner extends AbstractActor {
      * @param p Message containing accepted proposal
      */
     private void decided(final Acceptor.Proposal p) {
-        if (acceptorList.contains(sender())) {
-            accepted.put(sender(), p);
-            Map<Acceptor.Proposal, Integer> cts = new HashMap<>();
-            accepted.values()
-                    .forEach(pp -> cts.merge(pp, 1, Integer::sum));
-            Optional<Map.Entry<Acceptor.Proposal, Integer>> e =
-                    cts.entrySet().stream()
-                            .max(Comparator.comparingInt(Map.Entry::getValue));
-            if (e.isPresent() && e.get().getValue() > acceptorList.size() / 2) {
-                log.info("We have a chosen value: {}",
-                        e.get().getKey().getVal());
-            } else {
-                log.info("No evidence of chosen value yet");
-                scheduleCheckValue(duration);
-            }
-        } else {
+        if (!acceptorList.contains(sender())) {
             log.error("Recvd accepted proposal from acceptor not in our list");
+            return;
+        }
+
+        accepted.put(sender(), p);
+        Map<Acceptor.Proposal, Integer> cts = new HashMap<>();
+        accepted.values()
+                .forEach(pp -> cts.merge(pp, 1, Integer::sum));
+        Optional<Map.Entry<Acceptor.Proposal, Integer>> e =
+                cts.entrySet().stream()
+                        .max(Comparator.comparingInt(Map.Entry::getValue));
+        if (e.isPresent() && e.get().getValue() > acceptorList.size() / 2) {
+            log.info("We have a chosen value: {}",
+                    e.get().getKey().getVal());
+            chosenValue = e.get().getKey().getVal();
+        } else {
+            log.info("No evidence of chosen value yet");
         }
     }
 
